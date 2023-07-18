5f7156685e13e5e0ef25dc439e49cd1f3ac79c7f
Charles Buell <charles.buell@memverge.com>
2023-07-18 15:35:45 -0700

    Fixed average bests

--- benchmarks/stream/scripts/best_of.py
+++ benchmarks/stream/scripts/best_of.py
@@ -3,10 +3,16 @@ import sys
 
 
 def main(csv_file: str) -> None:
-    df = pd.read_csv(csv_file).iloc[:, 0:4]
+    df = (
+        pd.read_csv(csv_file)
+        .iloc[:, 0:4]
+        .groupby(["ArraySize", "Threads", "Function"])["BestRateMBs"]
+        .mean()
+        .reset_index()
+    )
 
-    df["Threads"], df["ArraySize"] = df["ArraySize"], df["Threads"]
-    df.columns = ["ArraySize", "Threads", "Function", "BestRateMBs"]
+    df["ArraySize"], df["Threads"] = df["Threads"], df["ArraySize"]
+    df.columns = ["Threads", "ArraySize", "Function", "BestRateMBs"]
 
     idx = df.groupby("ArraySize")["BestRateMBs"].idxmax()
     result_df = df.loc[idx]
