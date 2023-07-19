dd0e5ad88c32439ad0bea5d8b8c9f52b86dff39a
Charles Buell <charles.buell@memverge.com>
2023-07-19 11:49:02 -0700

    Migration from system args to argparse

--- benchmarks/stream/scripts/graph_csv.py
+++ benchmarks/stream/scripts/graph_csv.py
@@ -1,17 +1,41 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import sys
 import os
+from pathlib import Path
+import argparse
 
 
-def main(csv_file: str, folder: str) -> None:
+def file_exists(file: str) -> Path:
+    path = Path(file)
+
+    if not path.is_file():
+        raise argparse.ArgumentTypeError(f"File '{file}' does not exist.")
+
+    return path
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description='Create graphs from previously generated CSV files')
+
+    parser.add_argument('csv_file', type=file_exists,
+                        help='CSV file to process')
+
+    parser.add_argument('dir', type=file_exists,
+                        help='Directory to dump all the graphs into')
+
+    args = parser.parse_args()
+
+    csv_file = args.csv_file
+    dir = args.dir
+
     df = pd.read_csv(csv_file).iloc[:, 0:4]
 
     array_sizes = df["ArraySize"].drop_duplicates()
     functions = df["Function"].drop_duplicates()
 
-    if not os.path.isdir(folder):
-        os.makedirs(folder)
+    if not os.path.isdir(dir):
+        os.makedirs(dir)
 
     for array_size in array_sizes:
         filtered = df[df["ArraySize"] == array_size]
@@ -40,10 +64,10 @@ def main(csv_file: str, folder: str) -> None:
         plt.title(f"Array size: {array_size}")
         plt.legend()
 
-        f = folder + f"{array_size}.png"
+        f = dir + f"{array_size}.png"
         plt.savefig(f)
         plt.clf()
 
 
 if __name__ == "__main__":
-    main(csv_file=sys.argv[1], folder=sys.argv[2])
+    main()
