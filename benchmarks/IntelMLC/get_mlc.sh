5048d2e3847a6b0997a512c424dc8a94814c65b0
Bhanu Shankar <bhanu.shankar@memverge.com>
2024-03-21 10:41:51 -0700

    1. Change the autodownload of mlc to  version 3.11 2. Change the reporting of ratio from a raw value to quoted, which allows excel to show the ratio as is.

--- benchmarks/IntelMLC/get_mlc.sh
+++ benchmarks/IntelMLC/get_mlc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-wget https://downloadmirror.intel.com/763324/mlc_v3.10.tgz
-mkdir -p mlc_v3.10
-tar xf mlc_v3.10.tgz -C mlc_v3.10
-cp mlc_v3.10/Linux/mlc .
+wget https://downloadmirror.intel.com/793041/mlc_v3.11.tgz
+mkdir -p mlc_v3.11
+tar xf mlc_v3.11.tgz -C mlc_v3.11
+cp mlc_v3.11/Linux/mlc .
+rm mlc_v3.11.tgz
